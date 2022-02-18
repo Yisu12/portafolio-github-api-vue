7cd125fda01a0bdd71b7e9f4656f34a957cd92d5
Yisu12 <81430614+Yisu12@users.noreply.github.com>
2022-02-18 00:02:14 +0000

    v1 Portafolio GitHub
    Aplicación finalizada y desplegada.

---
+++ precache-manifest.872f6854fb73dff26ecc0b5a14bff570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab3eba6b980fcec017b",
+    "url": "/css/app.2c91ecb4.css"
+  },
+  {
+    "revision": "db5e1e07e46bc4e2f6e2a3c258475ca4",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "25db05bf8041192390205522a1101101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab3eba6b980fcec017b",
+    "url": "/js/app.1af95b90.js"
+  },
+  {
+    "revision": "fbe09261b8424a50ced2",
+    "url": "/js/chunk-vendors.f5c9e1b5.js"
+  },
+  {
+    "revision": "f269e12f3a0fb0f8f4f9df9b2125b763",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
